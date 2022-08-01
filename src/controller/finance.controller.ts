38017a00d18a20e3f2e5a9596b785578db1ec6ff
Nelson Gallego <nelson@remotepcsolutions.com>
2022-08-01 17:10:32 -0500

    Final endpoint income

--- src/controller/finance.controller.ts
+++ src/controller/finance.controller.ts
@@ -4,10 +4,11 @@ import FinanceModel from '../models/finance.model'
 export const income = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { walletId } = req.auth
-
     const { income } = req.body
+
     await FinanceModel.findByIdAndUpdate({ _id: walletId }, { income })
-    res.json('RUTA INCOME / INGRESOS')
+
+    res.json({ ok: true, message: 'Ingresos establecidos correctamente.' })
   } catch (e: any) {
     next(e)
   }
